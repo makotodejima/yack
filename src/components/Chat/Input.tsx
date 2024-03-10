7b7e02b244b2656356af02e31a56eb893ca2508f
Makoto Dejima <makotodejima@gmail.com>
2024-03-10 21:27:03 +0100

    autocorrect

--- src/components/Chat/Input.tsx
+++ src/components/Chat/Input.tsx
@@ -96,8 +96,6 @@ export const PromptInput = ({
         value={value}
         onChange={onChange}
         placeholder="Go on, ask me anything"
-        autoComplete="off"
-        autoCorrect="off"
         autoFocus
         onKeyDown={onKeyDown}
         rows={1}
